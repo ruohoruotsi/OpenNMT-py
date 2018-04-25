d3e71dce19df38975f8b9385b806116746397237
Sebastian Gehrmann <s.gehrmann@outlook.com>
2018-04-25 13:16:56 -0400

    fix inference

--- onmt/translate/Translator.py
+++ onmt/translate/Translator.py
@@ -149,7 +149,7 @@ class Translator(object):
             inp = inp.unsqueeze(2)
 
             # Run one step.
-            dec_out, dec_states, attn = self.model.decoder(
+            dec_out, dec_states, attn, tags = self.model.decoder(
                 inp, memory_bank, dec_states, memory_lengths=memory_lengths)
             dec_out = dec_out.squeeze(0)
             # dec_out: beam x rnn_size
--- translate.py
+++ translate.py
@@ -76,7 +76,8 @@ def main():
                                  window_size=opt.window_size,
                                  window_stride=opt.window_stride,
                                  window=opt.window,
-                                 use_filter_pred=False)
+                                 use_filter_pred=False,
+                                 tag_path=None)
 
     # Sort batch by decreasing lengths of sentence required by pytorch.
     # sort=False means "Use dataset's sortkey instead of iterator's".
